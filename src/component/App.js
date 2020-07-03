6c4810dc716b8db96e73c3dd2c80519180e50f98
Shivam Mishra <shivamtag40@gmail.com>
2020-07-03 06:51:42 +0530

    Set up the UI

--- src/component/App.js
+++ src/component/App.js
@@ -1,14 +1,25 @@
 import React, { Component } from 'react';
+import {data} from '../data';
+import Navbar from './Navbar';
+import MovieCard from './MovieCard';
 
-
-class App extends Component {
-  render() {
+function App() {
     return (
       <div className="App">
-       Project Setup
+       <Navbar />
+       <div className="main">
+         <div className="tabs">
+           <div className="tab">Movies</div>
+           <div className="tab">Favourites</div>
+         </div>
+         <div className="list">
+           {data.map(movie =>(
+             <MovieCard movie={movie} />
+           ))}
+         </div>
+       </div>
       </div>
     );
   }
-}
 
 export default App;
