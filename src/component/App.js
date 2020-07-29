ff2bda2647fdbe6aadfa48d19485f5816af2106e
Shivam Mishra <shivamtag40@gmail.com>
2020-07-29 18:34:35 +0530

    Changing the state to add favourites

--- src/component/App.js
+++ src/component/App.js
@@ -17,7 +17,8 @@ class App extends React.Component {
     console.log('STATE' ,this.props.store.getState());
   }
   render() {
-    const movies = this.props.store.getState();
+    const {list} = this.props.store.getState();//{list: [],favourite: []}
+    console.log('RENDER',this.props.store.getState());
     return (
       <div className="App">
         <Navbar />
@@ -27,7 +28,7 @@ class App extends React.Component {
             <div className="tab">Favourites</div>
           </div>
           <div className="list">
-            {movies.map((movie, index) => (
+            {list.map((movie, index) => (
               <MovieCard movie={movie} key={`movies-${index}`} />
             ))}
           </div>
